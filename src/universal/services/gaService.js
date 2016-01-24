7b80712d203ca980a59a8a33d307cdd39c0b5e93
Minkyu Cho <mrnoname@naver.com>
2016-01-24 11:32:57 +0900

    Add GA Initialize fail over

--- src/universal/services/gaService.js
+++ src/universal/services/gaService.js
@@ -1,15 +1,23 @@
 import { createReducer } from 'reduxible';
 import { UPDATE_PATH } from 'redux-simple-router';
+import config from '../../config/index';
 import ga from 'react-ga';
 
-ga.initialize(process.env.GA_TRACKING_ID || '');
+let reducer;
 
-export default createReducer({}, [
-  {
-    types: [UPDATE_PATH],
-    reduce: ({ payload : { path } }, state) => {
-      ga.pageview(path);
-      return state;
+if (config.server.current) {
+  reducer = createReducer({}, []);
+} else {
+  ga.initialize(process.env.GA_TRACKING_ID || '');
+  reducer = createReducer({}, [
+    {
+      types: [UPDATE_PATH],
+      reduce: ({ payload : { path } }, state) => {
+        ga.pageview(path);
+        return state;
+      }
     }
-  }
-]);
+  ]);
+}
+
+export default reducer;
